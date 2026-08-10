f501febf3c8a8481d9f2d2e188d2664ee2a95001
Jasim Mushtaq <jasimmushtaq786.com>
2026-08-10 11:13:31 +0530

    Fix Contact Us API URL for production

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -17,7 +17,7 @@ export default function ContactUs() {
     setSending(true);
 
     try {
-      const response = await fetch("http://localhost:3001/api/send-email", {
+      const response = await fetch("/api/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
